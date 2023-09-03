1a4bc83a5da1097ed9199c374171e2be8a43504e
Graham Preston <graham.preston@gmail.com>
2023-09-03 12:12:46 -0400

    Preliminary PhoenixBackendClient and SlippiClient

---
+++ src/main/PhoenixBackendClient.ts
@@ -0,0 +1,107 @@
+// Types
+
+import { PlayerType } from '@slippi/slippi-js';
+
+// Events the frontend can add handlers for
+enum PhoenixEventType {
+  CHANNEL_JOINED = 'CHANNEL_JOINED',
+  CHANNEL_JOIN_ERROR = 'CHANNEL_JOIN_ERROR'
+}
+
+type PhoenixChannelJoinedEvent = {
+  type: PhoenixEventType.CHANNEL_JOINED;
+  topic: string;
+  clientCode: string;
+};
+
+type PhoenixChannelJoinErrorEvent = {
+  type: PhoenixEventType.CHANNEL_JOIN_ERROR;
+  topic: string;
+  error: any;
+};
+
+type PhoenixEventMap = {
+  [PhoenixEventType.CHANNEL_JOINED]: PhoenixChannelJoinedEvent;
+  [PhoenixEventType.CHANNEL_JOIN_ERROR]: PhoenixChannelJoinErrorEvent;
+};
+
+type PhoenixEvent = PhoenixEventMap[PhoenixEventType];
+
+type PhoenixBinding<T extends PhoenixEventType> = {
+  eventType: T;
+  callback: (event: PhoenixEventMap[T]) => void;
+};
+
+export interface PhoenixService {
+  connect(): void;
+  onEvent(eventType: PhoenixEventType, handle: (event: PhoenixEventMap[typeof eventType]) => void): void;
+  gameStarted(players: PlayerType[]): void;
+}
+
+// Implementation
+
+import { Socket, Channel } from 'phoenix-channels';
+
+const SOCKET_URL = 'ws://127.0.0.1:4000/socket';
+const CHANNEL_TOPIC = 'clients';
+
+type ClientChannelConnectResponse = { client_code: string };
+
+export class PhoenixBackendService implements PhoenixService {
+  socket: typeof Socket;
+  channel: typeof Channel;
+
+  private clientCode: string | undefined;
+  private bindings: Array<PhoenixBinding<PhoenixEventType>>;
+
+  constructor(token: string) {
+    this.socket = new Socket(SOCKET_URL, { params: { client_token: token } });
+    this.channel = this.socket.channel(CHANNEL_TOPIC, {});
+
+    this.bindings = [];
+  }
+
+  gameStarted(players: PlayerType[]) {
+    this.channel.isJoined() && this.channel.push('game_started', {
+      client: this.clientCode!,
+      players: players.map(p => p.connectCode).sort()
+    });
+  }
+
+  connect() {
+    this.socket.connect();
+
+    this.channel.join()
+      .receive('ok', (resp: ClientChannelConnectResponse) => {
+        console.log('Joined successfully, reply:', resp);
+        this.clientCode = resp.client_code;
+        // UserData.writeData('client-code', resp.connect_code);
+
+        this.handleEvent({
+          type: PhoenixEventType.CHANNEL_JOINED,
+          topic: this.channel.topic,
+          clientCode: resp.client_code
+        });
+      })
+      .receive('error', (resp: any) => {
+        console.log('Unable to join:', resp);
+
+        this.handleEvent({
+          type: PhoenixEventType.CHANNEL_JOIN_ERROR,
+          topic: this.channel.topic,
+          error: resp
+        });
+      });
+  }
+
+  onEvent(eventType: PhoenixEventType, handle: (event: PhoenixEventMap[typeof eventType]) => void): void {
+    this.bindings.push({ eventType, callback: handle })
+  }
+
+  // TODO: sendEvent, takes an outward facing event (game start) and forwards to the backend
+
+  private handleEvent(event: PhoenixEvent): void {
+    this.bindings.filter((bind) => bind.eventType === event.type)
+      .forEach((bind) => { bind.callback(event); });
+  }
+}
---
+++ src/main/SlippiClient.ts
@@ -0,0 +1,89 @@
+import { ConnectionStatus } from '@slippi/slippi-js';
+
+// Types
+
+// Events that the frontend can add handlers for
+enum SlippiEventType {
+  CONNECTION_STATUS_CHANGED = 'CONNECTION_STATUS_CHANGED'
+}
+
+type SlippiConnectionStatusChangedEvent = {
+  type: SlippiEventType.CONNECTION_STATUS_CHANGED;
+  status: ConnectionStatus;
+};
+
+type SlippiEventMap = {
+  [SlippiEventType.CONNECTION_STATUS_CHANGED]: SlippiConnectionStatusChangedEvent;
+};
+
+type SlippiEvent = SlippiEventMap[SlippiEventType];
+
+type SlippiBinding<T extends SlippiEventType> = {
+  eventType: T;
+  callback: (event: SlippiEventMap[T]) => void;
+};
+
+interface SlippiService {
+  connect(): void;
+  onEvent(eventType: SlippiEventType, handle: (event: SlippiEventMap[typeof eventType]) => void): void;
+}
+
+// Implementation
+
+import { Ports, ConnectionEvent } from '@slippi/slippi-js';
+import { SlpLiveStream, SlpRealTime } from '@vinceau/slp-realtime';
+import { PhoenixService } from './PhoenixBackendClient';
+
+const SLIPPI_ADDRESS = '127.0.0.1';
+const SLIPPI_PORT = Ports.DEFAULT;
+const CONNECTION_TYPE = 'dolphin';
+
+export class SlippiClient implements SlippiService {
+  backendClient: PhoenixService;
+  livestream: SlpLiveStream;
+
+  private bindings: Array<SlippiBinding<SlippiEventType>>;
+
+  constructor(backendClient: PhoenixService) {
+    this.backendClient = backendClient;
+    this.bindings = [];
+
+    this.livestream = new SlpLiveStream(CONNECTION_TYPE);
+    const realtime = new SlpRealTime();
+    realtime.setStream(this.livestream);
+
+    // Connect to Slippi
+    this.livestream.connection.on(ConnectionEvent.STATUS_CHANGE, (status) => {
+      console.log('got slippi status change', status);
+
+      this.handleEvent({
+        type: SlippiEventType.CONNECTION_STATUS_CHANGED,
+        status
+      });
+    });
+
+    // Handle game events
+    realtime.game.start$.subscribe((payload) => {
+      this.backendClient.gameStarted(payload.players);
+    });
+  }
+
+  connect() {
+    this.livestream.start(SLIPPI_ADDRESS, SLIPPI_PORT)
+      .then(() => {
+        console.log('in slippi livestream.start.then');
+      })
+      .catch(() => {
+        console.log('in slippi livestream.start.catch');
+      });
+  }
+
+  onEvent(eventType: SlippiEventType, handle: (event: SlippiEventMap[typeof eventType]) => void): void {
+    this.bindings.push({ eventType, callback: handle })
+  }
+
+  private handleEvent(event: SlippiEvent): void {
+    this.bindings.filter((bind) => bind.eventType === event.type)
+      .forEach((bind) => { bind.callback(event); });
+  }
+}
