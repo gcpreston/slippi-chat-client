a778626647db9a8de296b66a03f646366a27ad01
Graham <graham.preston@gmail.com>
2023-09-08 07:01:57 -0700

    Copy token to clipboard

--- src/renderer/components/Token.tsx
+++ src/renderer/components/Token.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, TextField } from '@mui/material';
+import { Button, Snackbar } from '@mui/material';
 import Check from '@mui/icons-material/Check';
 import Cancel from '@mui/icons-material/Cancel';
 
@@ -9,6 +9,23 @@ import TokenInput from './TokenInput';
 const Token = () => {
   const [token, setToken] = useToken();
   const [editing, setEditing] = useState(false);
+  const [snackOpen, setSnackOpen] = useState(false);
+
+  const handleClose = (_event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackOpen(false);
+  };
+
+  const copyToken = () => {
+    const type = "text/plain";
+    const blob = new Blob([token || ''], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+
+    return navigator.clipboard.write(data);
+  };
 
   return (
     <>
@@ -19,6 +36,7 @@ const Token = () => {
             setToken(newToken);
             setEditing(false);
           }}
+          cancel={() => setEditing(false)}
         />
         :
         <div className='flex space-x-4'>
@@ -26,16 +44,26 @@ const Token = () => {
             <span>
               <Check />
               Token set
-              </span>
+            </span>
             :
             <span>
               <Cancel />
               No token set
             </span>
           }
-          <Button variant="contained" onClick={() => setEditing(true)}>
+          <Button variant='contained' onClick={() => setEditing(true)}>
             Edit
           </Button>
+          <Button variant='outlined' onClick={() => copyToken().then(() => setSnackOpen(true))}>
+            Copy
+          </Button>
+
+          <Snackbar
+            open={snackOpen}
+            autoHideDuration={4000}
+            onClose={handleClose}
+            message="Copied to clipboard"
+          />
         </div>
       }
     </>
